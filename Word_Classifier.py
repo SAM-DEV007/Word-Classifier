b4f9b8e003c8b2fc16cbce49308a2ef69468850f
SAM <60264918+SAM-DEV007@users.noreply.github.com>
2023-07-07 17:15:38 +0530

    Update Word_Classifier.py

--- Word_Classifier.py
+++ Word_Classifier.py
@@ -15,7 +15,7 @@ def download_model(id, destination):
     Downloads and saves the model.
 
     If the model fails to download, it can be downloaded manually:
-    https://drive.google.com/file/d/1DevltQPYppcpk_83ZToJLia6_WVIOVno/view?usp=sharing
+    https://drive.google.com/file/d/1LAiyCV0p6v-lROdXbtrzKlF4APNmM3Qm/view?usp=sharing
     '''
 
     URL = "https://docs.google.com/uc?export=download"
@@ -52,14 +52,14 @@ if __name__ == '__main__':
     model_path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__))) + '\\Model\\Model_Data', 'WordClassifier_Model.h5')
 
     if not os.path.exists(model_path):
-        download_model('1DevltQPYppcpk_83ZToJLia6_WVIOVno', model_path)
+        download_model('1LAiyCV0p6v-lROdXbtrzKlF4APNmM3Qm', model_path)
     
     model = tf.keras.models.load_model(model_path, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
     txt = input("Enter your message: ")
 
     result_arg = (np.squeeze(model.predict([txt])))
     result = np.argmax(result_arg)
-    txt_arg = ["N/A", "Saying Hi", "Borrow Money"]
+    txt_arg = ["N/A", "Saying Hi", "Borrow Money", "YouTube Interaction"]
 
     print(f"\nThe prediction is based between - {' | '.join(txt_arg)}.\n")
     print(f"The prediction is {txt_arg[result]}, with the probability of {np.round(result_arg[result]*100, 2)}%")
